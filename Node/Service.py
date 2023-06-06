ebf567bc26d2d48945be97161f83aa04ad5562fb
Denis Figueiredo <135567383+DenisFerreiraFigueiredo@users.noreply.github.com>
2023-06-06 12:15:14 -0300

    Add files via upload

---
+++ Node/Service.py
@@ -0,0 +1,134 @@
+#
+#
+#
+
+import sys
+import os
+from pathlib import Path
+
+ipth = Path(__file__).parent.parent
+ipth = str(ipth)
+
+if ipth not in sys.path:
+    sys.path.insert(0, ipth)
+
+from Node import Words as _w
+
+#from Node import Node
+
+class Service():
+    
+    class serviceConfig():
+        
+        def __init__(self, parent, path):
+            self._Parent=parent
+            self.Cfg = File.Output(path, mkdir=true)
+            return
+            
+        def close(self):
+            if self.Cfg is not None:
+                self.skip()
+                self.end()
+                self.Cfg.close()
+                self.Cfg=None            
+            return self
+        
+        def __call__(self, var, *val):
+            if self.Cfg is not None:
+                if var is list:
+                    var = var.join("-")
+                self._set(var, *val)
+            return self
+            
+        def _set(self, var, *val):
+            self.Cfg(var, "= ", *val)
+            return self
+           
+        def skip(self):
+            if self.Cfg is not None:
+               self.Cfg()
+            return self
+           
+        def remark(self, *s):
+            if self.Cfg is not None:
+                self.Cfg("# ", *s)
+            return self
+            
+        def __enter__(self): 
+            return self 
+            
+            
+        def __exit__(self, type, value, tb): 
+            self.Cfg.close()
+            return
+        
+        
+        def end(self):
+            return self
+            
+        pass
+
+    def __init__(self, instance=_w.Default):
+        self.Instance=instance.title()
+        self.Cfg = None
+        return
+        
+    @property 
+    def configSufix(self):
+        return ".conf"
+                      
+    def configPath(self, *args):
+        return Node.tempPath(self.__class__.__name, *args)
+                
+    @property 
+    def configFilePath(self):
+        p=self.configPath / (self.Instance+self.configSufix)
+        return p
+        
+    configFileClass = serviceConfig
+           
+    def begin(self):
+        if self._Config is None:
+            self._Config= configFileClass(self, self.configFilePath)
+        return self._Config
+                
+    def end(self):
+        if self._Config is not None:
+            self._Config.close()
+        return self    
+        
+    def start(self):
+        self.stop()
+        self.prepare()
+        self.config()
+        self.run()
+        return self
+        
+    def run(self):
+        return self
+        
+    def stop(self):
+        return self
+        
+    def restart(self):
+        return self
+                
+    def config(self):
+        self.begin()
+        
+        self.end()                	
+        return self
+
+    def info(self):
+        return
+        
+    def status(self):
+        return False
+        
+    def version(self):
+        return (0, 0, 0)
+
+    pass
+
+
+###
